cb0d0edbf04241f75e329fd67a05d925218513f4
Yoshiki Yamada <nyamadandan@gmail.com>
2014-06-21 17:16:46 +0900

    ヒーローではないエンティティを追加

--- game/scripts/archetypes/entities/ledo.js
+++ game/scripts/archetypes/entities/ledo.js
@@ -46,3 +46,27 @@ EntityFactory.archetypes.a_ledo =
         ambientEffect: 'sfx_dust'
     }
 };
+
+EntityFactory.archetypes.a_friend =
+{
+  shouldLevelSave: false,
+  shouldPlayerSave: false,
+
+  ECMesh :
+  {
+    scale       :   1.0,
+    scaleX      :   1.0,
+    scaleY      :   1.0,
+    scaleZ      :   1.0,
+    offsetY     :   1.0,
+    rotationX   :   0.0,
+    path        :   'characters/ledo_kite.dae',
+    localOffset :   [0.0, -1.0, 0.0],
+    foreground : true
+  },
+
+  ECAnimationManager : {
+    animationSet : 'ledo_kite'
+  }
+};
+
